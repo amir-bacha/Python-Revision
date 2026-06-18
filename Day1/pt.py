d948d377c7d9e63c90bce3d3cf9734320952b491
amir bacha <ab963068@gmail.com>
2026-06-18 04:33:13 -0700

    day 2

--- Day1/pt.py
+++ Day1/pt.py
@@ -1,17 +1,4 @@
-# String
-# String is data type that store  sequence of character e.g name="amir bacha"
-#  if a string have five character then five size memory is allocated in memory
-#   Note: spaces are  also counted
-# string have indexes like array and starts from 0 
-# string are immutable 
-name ="Amir bacha"
-print(type(name)),  # string 
-print(len(name)) # 10 bc space is also counted
-print (name[0])
-# name[2]="k" this is not allowed bc string are not mutable
-
-# Basic operation  on string 
-
-string = "Hello" + "world"   # concatenation 
-print(string) # Hello world
-
+# slicing 
+name="Amir bacha" 
+ #  name=name[::-1]  reverse of string 
+print (name[2:4]) # ir
