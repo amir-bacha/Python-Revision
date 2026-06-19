33f7c4a6435f387c7585fe964127a062e867ca02
amir bacha <ab963068@gmail.com>
2026-06-19 03:39:42 -0700

    Sets and function on it

--- Day1/pt.py
+++ Day1/pt.py
@@ -1,4 +1,8 @@
-# slicing 
-name="Amir bacha" 
- #  name=name[::-1]  reverse of string 
-print (name[2:4]) # ir
+set1={2,3,4,5}
+set2={3,6,7,9}
+print(set1|set2) #union
+print(set1&set2) #intersection
+print(set1-set2) # difference 2,5
+print(set1^set2) # 2,4,5,6,7,9
+print(set1.issubset(set2)) # false
+print(set2.issuperset(set1)) # false
